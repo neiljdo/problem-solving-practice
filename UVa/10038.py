c74fd2dbfa121729d6071233ca57ad43b1c5976f
Neil John D. Ortega <neiljohn.ortega+git@gmail.com>
2020-08-24 22:10:34 +0900

    Create 10038.py

---
+++ UVa/10038.py
@@ -0,0 +1,23 @@
+import sys
+import math
+
+
+def main():
+    for line in sys.stdin:
+        case = list(map(int, line.strip().split()))
+        n = case[0]
+        nums = case[1:]
+
+        found = [False] * (n - 1)
+        jolly = False
+        for i in range(n-1):
+            diff = int(abs(nums[i] - nums[i+1]))
+            if diff not in range(1, n): break
+            found[diff - 1] = True
+
+        jolly = all(found)
+        if jolly: print("Jolly")
+        else: print("Not jolly")
+
+if __name__ == "__main__":
+    main()
